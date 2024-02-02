166d40dd793ed27ea4b5f04a489471edc23748b5
sidharth-git <sidharthmishra.ac@gmail.com>
2024-02-02 05:35:27 +0530

    source code added

---
+++ src/test/java/com/example/mypacakage/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypacakage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
